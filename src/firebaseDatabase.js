3a24275e1171c195e8f670a908e44b0079a8283d
Surendra Verma <62333368+surrenderoz@users.noreply.github.com>
2024-01-17 09:22:13 -0800

    Update firebaseDatabase.js

--- src/firebaseDatabase.js
+++ src/firebaseDatabase.js
@@ -1,16 +1,1 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAx2FSU2bivPPz7pS71jUY1a52wtcghwoI',
-  authDomain: 'caringtogether-244ad.firebaseapp.com',
-  projectId: 'caringtogether-244ad',
-  storageBucket: 'caringtogether-244ad.appspot.com',
-  messagingSenderId: '968205073513',
-  appId: '1:968205073513:web:15361c8190ac003e8e76f8',
-  measurementId: 'G-PW4ZR9J43P'
-}
-
-const firebaseApp = firebase.initializeApp(firebaseConfig)
-
-export const db = firebaseApp.firestore()
